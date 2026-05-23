aff0f13dcff3f75e0ae3a97513eefe749c607a19
javvivie <jaaravena2025@udec.cl>
2026-05-22 23:56:13 -0400

    feat: se agrego getCorreo() a la interfaz Invitable

--- src/main/java/com/reuniones/Invitable.java
+++ src/main/java/com/reuniones/Invitable.java
@@ -1,7 +1,7 @@
 package com.reuniones;
 
 /**
- * Interfaz que representa cualquier entidad que pued ser invitada a una reunión.
+ * Interfaz que representa cualquier entidad que puede ser invitada a una reunión.
  */
 
 public interface Invitable {
@@ -9,4 +9,9 @@ public interface Invitable {
      * Envía una invitación a la entidad.
      */
     void invitar();
+
+    /**
+     * Obtiene el correo del invitado para revisar su asistencia.
+     */
+    String getCorreo();
 }
